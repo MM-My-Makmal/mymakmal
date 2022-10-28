cb721d65fc11b627e18eefe67ab0954c0b9ce2b3
aida <aidazamziela@gmail.com>
2022-10-28 11:24:23 +0800

    addition

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
-use App\Http\Controllers\HomeController;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
